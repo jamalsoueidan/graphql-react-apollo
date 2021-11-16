fcae56bf1618b7817c4f07d8cb321db54f0b8046
Jamal Soueidan <jamal@soueidan.com>
2021-11-16 11:38:08 +0100

    fixing checkbox value in formik

--- src/components/organization/add.tsx
+++ src/components/organization/add.tsx
@@ -1,26 +1,29 @@
 import Button from "@mui/material//Button";
 import TextField from "@mui/material//TextField";
-import { Form, Formik } from "formik";
 import FormGroup from "@mui/material/FormGroup";
+import { Form, Formik, FormikProps, useField } from "formik";
+import * as queries from "../../generated";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import * as queries from "../../generated";
 
 interface MyFormValues {
   name: string;
   users: string[];
 }
 
+const MyCheckbox = ({ label, user, ...props }: any) => {
+  const [field] = useField(props);
+  return (
+    <FormControlLabel
+      control={<Checkbox {...field} {...props} />}
+      label={label}
+    />
+  );
+};
+
 const AddOrganization = () => {
-  const initialValues: MyFormValues = { name: "", users: ["1"] };
+  const initialValues: MyFormValues = { name: "", users: [] };
   const { data, error, loading } = queries.useUsersQuery();
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      //arr.push(value);
-    } else {
-      //arr.splice(arr.indexOf(value), 1);
-    }
-  };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -31,44 +34,43 @@ const AddOrganization = () => {
   }
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={(values, actions) => {
-        console.log({ values, actions });
-        alert(JSON.stringify(values, null, 2));
-        actions.setSubmitting(false);
-      }}
-    >
-      <Form>
-        <TextField
-          fullWidth
-          id="name"
-          name="name"
-          label="Name"
-          autoComplete="off"
-        />
-        <FormGroup>
-          {data.users?.map((user) => (
-            <FormControlLabel
-              key={user?.id}
-              control={
-                <Checkbox
-                  name="users"
-                  id="users"
-                  value={user?.id || ""}
-                  onChange={handleChange}
-                />
-              }
-              label={user?.name || ""}
+    <div>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values, actions) => {
+          console.log({ values, actions });
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }}
+      >
+        {(props: FormikProps<MyFormValues>) => (
+          <Form>
+            <TextField
+              fullWidth
+              id="name"
+              name="name"
+              label="Name"
+              autoComplete="off"
             />
-          ))}
-        </FormGroup>
 
-        <Button color="primary" variant="contained" fullWidth type="submit">
-          Submit
-        </Button>
-      </Form>
-    </Formik>
+            <FormGroup>
+              {data.users?.map((user) => (
+                <MyCheckbox
+                  key={user?.id}
+                  name="users"
+                  label={user?.name}
+                  value={user?.id}
+                />
+              ))}
+            </FormGroup>
+
+            <Button color="primary" variant="contained" fullWidth type="submit">
+              Submit
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 };
 
